ae0265deeeb4329ed7f5013258955aae78ee4026
devFarjanur <farjanur15-2956@diu.edu.bd>
2023-12-21 01:08:31 +0600

    get service from server to client side

--- index.js
+++ index.js
@@ -73,6 +73,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/service/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/service', async(req, res) => {
       const addService = req.body;
       const result = await serviceCollection.insertOne(addService);
